22caf5271f6d8868cfe2b1cb86e3d085db13b49b
xiaoyaocode163 <60478717+xiaoyaocode163@users.noreply.github.com>
2021-05-09 04:45:58 +0800

    Delete Ver5.5.7 directory

--- Ver5.5.7/MakeOnezip.bat
+++
@@ -1,1 +0,0 @@
-copy /B VisualFreeBasic5.5.7.zip.001 + VisualFreeBasic5.5.7.zip.002 + VisualFreeBasic5.5.7.zip.003 + VisualFreeBasic5.5.7.zip.004 + VisualFreeBasic5.5.7.zip.005 + VisualFreeBasic5.5.7.zip.006 + VisualFreeBasic5.5.7.zip.007 + VisualFreeBasic5.5.7.zip.008 #VFB5.5.7.ZIP
