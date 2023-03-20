5f6ccc40dc91491cd102cd66c04526be9cd3df1e
xiaoyaocode163 <60478717+xiaoyaocode163@users.noreply.github.com>
2023-03-20 17:56:18 +0800

    Delete 涓嬭浇 directory

--- 下载/MakeToZip.bat
+++
@@ -1,1 +0,0 @@
-copy /B VisualFreeBasic5.zip.001 + VisualFreeBasic5.zip.002 + VisualFreeBasic5.zip.003 + VisualFreeBasic5.zip.004 + VisualFreeBasic5.zip.005 + VisualFreeBasic5.zip.006 + VisualFreeBasic5.zip.007 #VisualFreeBasic5.2023.zip
