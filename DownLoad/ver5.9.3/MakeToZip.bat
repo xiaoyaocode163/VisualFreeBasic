010bb84f8b0defe63c5950f49652d1f2ccb70d6d
xiaoyaocode163 <60478717+xiaoyaocode163@users.noreply.github.com>
2024-11-06 09:15:43 +0800

    Add files via upload

---
+++ DownLoad/ver5.9.3/MakeToZip.bat
@@ -0,0 +1,1 @@
+copy /B VisualFreeBasic593.zip.001 + VisualFreeBasic593.zip.002 + VisualFreeBasic593.zip.003 + VisualFreeBasic593.zip.004 + VisualFreeBasic593.zip.005 + VisualFreeBasic593.zip.006 + VisualFreeBasic593.zip.007 "#VFB593.zip"
